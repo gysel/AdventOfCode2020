a450356465db1dcd66cd17c053cf17d609ee3693
Michael Gysel <michael.gysel@unblu.com>
2020-12-02 07:22:59 +0100

    Solve day 1 using a sequence

--- src/main/kotlin/ch/mgysel/aoc/day01/Run.kt
+++ src/main/kotlin/ch/mgysel/aoc/day01/Run.kt
@@ -7,6 +7,7 @@ import ch.mgysel.aoc.common.printAndMeasureDuration
 fun main() {
     printAndMeasureDuration("Part One", ::solvePart1)
     printAndMeasureDuration("Part Two", ::solvePart2)
+    printAndMeasureDuration("Part Two (Sequence approach)", ::solvePart2usingSequence)
 }
 
 val numbers = InputData.readLines("day01.txt").map { it.toInt() }
@@ -17,6 +18,19 @@ fun solvePart1(): Int {
 
 fun solvePart2(): Int {
     return findCombinationWithSum(numbers, 2020, 3).product()
+}
+
+fun solvePart2usingSequence(): Int {
+    val combinations = sequence {
+        numbers.forEach { a ->
+            numbers.forEach { b ->
+                numbers.forEach { c ->
+                    yield(listOf(a, b, c))
+                }
+            }
+        }
+    }
+    return combinations.first { it.sum() == 2020 }.product()
 }
 
 fun List<Int>.product() = reduce(Int::times)
