c2c5dc761b83b6f49f88afa979c836e2cd43f766
Michael Gysel <michael.gysel@unblu.com>
2020-12-05 15:20:53 +0100

    Add template

---
+++ src/main/kotlin/ch/mgysel/aoc/template/Run.kt
@@ -0,0 +1,14 @@
+package ch.mgysel.aoc.template
+
+import ch.mgysel.aoc.common.printAndMeasureDuration
+
+fun main() {
+    printAndMeasureDuration("Part One", ::solvePart1)
+    printAndMeasureDuration("Part Two", ::solvePart2)
+}
+
+//val data = InputData.readLines("day00.txt")
+
+fun solvePart1(): Any = "TODO"
+
+fun solvePart2(): Any = "TODO"
