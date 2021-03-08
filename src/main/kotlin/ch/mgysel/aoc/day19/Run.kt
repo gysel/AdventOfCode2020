b52d4f42f8c1307911250d1c1a80219ecea0e187
Michi Gysel <mail@mgysel.ch>
2021-03-08 21:22:22 +0100

    Solve part two with a little bit of help...

--- src/main/kotlin/ch/mgysel/aoc/day19/Run.kt
+++ src/main/kotlin/ch/mgysel/aoc/day19/Run.kt
@@ -1,7 +1,6 @@
 package ch.mgysel.aoc.day19
 
 import ch.mgysel.aoc.common.InputData
-import ch.mgysel.aoc.common.cartesianProduct
 import ch.mgysel.aoc.common.printAndMeasureDuration
 
 fun main() {
@@ -10,50 +9,66 @@ fun main() {
 }
 
 val data = InputData.readLines("day19.txt")
+val modifications = """
+            8: 42 | 42 8
+            11: 42 31 | 42 11 31
+        """.trimIndent().lines().parseRules()
 
 fun solvePart1(): Any {
     val (rules, messages) = data.parse()
-    val matchingMessages = rules.calculatePossibleMessages("0")
-    return messages.count { it in matchingMessages }
+    return messages.count { message -> rules.ruleMatch("0", message) }
+}
+
+fun solvePart2(): Any {
+    val (rules, messages) = data.parse()
+    val newRules = rules + modifications
+    return messages.count { message -> newRules.ruleMatch("0", message) }
 }
 
 fun List<String>.parse(): Pair<Map<String, String>, List<String>> {
-    val rules = this
+    val rules = parseRules()
+    val messages = drop(rules.size + 1)
+    return rules to messages
+}
+
+private fun List<String>.parseRules() = this
         .takeWhile(String::isNotBlank)
         .map { rawRule ->
             val (id, instructions) = rawRule.split(": ", limit = 2)
             id to instructions
         }.toMap()
 
-    val messages = data.drop(rules.size + 1)
-    return rules to messages
-}
+fun Map<String, String>.ruleMatch(ruleId: String, message: String) =
+        this.ruleMatch(ruleId, message, 0).any { it == message.length }
 
 /**
- * Set: Can be any of...
+ * Adjusted version of https://todd.ginsberg.com/post/advent-of-code/2020/day19/
  */
-fun Map<String, String>.calculatePossibleMessages(ruleId: String): Set<String> {
-    val rule = this[ruleId] ?: throw IllegalStateException("No rule with id $ruleId!")
-    val tokens = rule.split(" ")
-    val processToken = fun(token: String): Set<String> {
-        return if (token.startsWith("\"")) {
-            setOf(token.drop(1).dropLast(1))
-        } else {
-            this.calculatePossibleMessages(token)
+private fun Map<String, String>.ruleMatch(ruleId: String, message: String, position: Int): List<Int> =
+        getValue(ruleId).split(" ").splitOr().flatMap { listOfRules -> // OR Rule
+            var positions = listOf(position)
+            listOfRules.forEach { rule ->  // AND Rule
+                positions = positions.mapNotNull { idx ->
+                    when {
+                        rule.startsWith("\"") && message.getOrNull(idx).toString() == rule.removeQuotes() ->
+                            listOf(idx + 1) // End condition
+                        !rule.startsWith("\"") ->
+                            ruleMatch(rule, message, idx)
+                        else ->
+                            null
+                    }
+                }.flatten()
+            }
+            positions
         }
-    }
-    val combine = fun(input: List<String>): Set<String> {
-        // can the cartesian product accept an Iterable?
-        return cartesianProduct(input.map { processToken(it).toList() })
-            .map { it.joinToString("") }.toSet()
-    }
-    return if (tokens.contains("|")) {
-        val left = tokens.takeWhile { it != "|" }
-        val right = tokens.drop(left.size + 1)
-        combine(left) + combine(right)
-    } else {
-        combine(tokens)
-    }
+
+private fun List<String>.splitOr(): List<List<String>> {
+    return if (this.contains("|")) {
+        val left = this.takeWhile { it != "|" }
+        listOf(left, drop(left.size + 1))
+    } else listOf(this)
 }
 
-fun solvePart2(): Any = "TODO"
+private fun String.removeQuotes(): String {
+    return drop(1).dropLast(1)
+}
--- src/test/kotlin/ch/mgysel/aoc/day19/Day19Test.kt
+++ src/test/kotlin/ch/mgysel/aoc/day19/Day19Test.kt
@@ -8,7 +8,7 @@ import io.kotest.matchers.shouldBe
 
 class Day19Test : StringSpec({
 
-    val sample = """
+    val firstExample = """
         0: 1 2
         1: "a"
         2: 1 3 | 3 1
@@ -16,24 +16,98 @@ class Day19Test : StringSpec({
     """.trimIndent().lines()
 
     "verify example of part 1" {
-        val (rules, messages) = sample.parse()
+        val (rules, _) = firstExample.parse()
         rules should haveSize(4)
-        rules.calculatePossibleMessages("1") shouldBe setOf("a")
-        rules.calculatePossibleMessages("3") shouldBe setOf("b")
-        rules.calculatePossibleMessages("2") shouldBe setOf("ab", "ba")
-        rules.calculatePossibleMessages("0") shouldBe setOf("aab", "aba")
+        rules.ruleMatch("0", "aab") shouldBe true
+        rules.ruleMatch("0", "aba") shouldBe true
+        rules.ruleMatch("0", "aaa") shouldBe false
+        rules.ruleMatch("0", "abab") shouldBe false
+    }
+
+    val secondExample = """
+        0: 4 1 5
+        1: 2 3 | 3 2
+        2: 4 4 | 5 5
+        3: 4 5 | 5 4
+        4: "a"
+        5: "b"
+
+        ababbb
+        bababa
+        abbbab
+        aaabbb
+        aaaabbb
+    """.trimIndent().lines()
+
+    "verify second example of part 1" {
+        val (rules, messages) = secondExample.parse()
+        val matching = messages.filter { rules.ruleMatch("0", it) }
+        matching shouldContainExactly listOf("ababbb", "abbbab")
     }
 
     "verify solution of part 1" {
         solvePart1() shouldBe 120
     }
 
+    val examplePartTwo = """
+        42: 9 14 | 10 1
+        9: 14 27 | 1 26
+        10: 23 14 | 28 1
+        1: "a"
+        11: 42 31
+        5: 1 14 | 15 1
+        19: 14 1 | 14 14
+        12: 24 14 | 19 1
+        16: 15 1 | 14 14
+        31: 14 17 | 1 13
+        6: 14 14 | 1 14
+        2: 1 24 | 14 4
+        0: 8 11
+        13: 14 3 | 1 12
+        15: 1 | 14
+        17: 14 2 | 1 7
+        23: 25 1 | 22 14
+        28: 16 1
+        4: 1 1
+        20: 14 14 | 1 15
+        3: 5 14 | 16 1
+        27: 1 6 | 14 18
+        14: "b"
+        21: 14 1 | 1 14
+        25: 1 1 | 1 14
+        22: 14 14
+        8: 42
+        26: 14 22 | 1 20
+        18: 15 15
+        7: 14 5 | 1 21
+        24: 14 1
+
+        abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+        bbabbbbaabaabba
+        babbbbaabbbbbabbbbbbaabaaabaaa
+        aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+        bbbbbbbaaaabbbbaaabbabaaa
+        bbbababbbbaaaaaaaabbababaaababaabab
+        ababaaaaaabaaab
+        ababaaaaabbbaba
+        baabbaaaabbaaaababbaababb
+        abbbbabbbbaaaababbbbbbaaaababb
+        aaaaabbaabaaaaababaa
+        aaaabbaaaabbaaa
+        aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+        babaaabbbaaabaababbaabababaaab
+        aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+    """.trimIndent().lines()
+
     "verify examples of part 2" {
+        val (rules, messages) = examplePartTwo.parse()
+        messages.count { rules.ruleMatch("0", it) } shouldBe 3
+        val newRules = rules + modifications
+        messages.count { newRules.ruleMatch("0", it) } shouldBe 12
     }
 
-
     "verify solution of part 2" {
-        solvePart2() shouldBe "TODO"
+        solvePart2() shouldBe 350
     }
 
 
