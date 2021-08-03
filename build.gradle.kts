4e29b536b617ac506c5075bdeda662f4dd1c88a9
Michael Gysel <michael.gysel@unblu.com>
2021-08-03 13:33:21 +0200

    Update Kotlin, Gradle, Kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,18 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.jetbrains.kotlin.jvm").version("1.4.21")
+    id("org.jetbrains.kotlin.jvm").version("1.5.21")
 }
 
 repositories {
-    jcenter()
+    mavenCentral()
 }
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
 
-    val kotestVersion = "4.3.1"
+    val kotestVersion = "4.6.1"
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
     testImplementation("io.kotest:kotest-property:$kotestVersion")
